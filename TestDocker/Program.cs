9a409c64b8891d9478e7ff7ee7fa1df9311f47f8
Lasse Rolstad <lasse@truegroups.com>
2018-01-12 14:35:54 +0100

    Add project files.

---
+++ TestDocker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
